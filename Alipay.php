9f63a47616ff392aa91f38a7e7c6dab21c6c709a
tekadmin <tekadmin@163.com>
2015-09-10 16:56:15 +0800

    修复即时到账接口

--- Alipay.php
+++ Alipay.php
@@ -24,103 +24,56 @@ use mkui\alipay\Aliconfig;
 /* * ************************请求参数************************* */
 
 class Alipay {
+    //支付类型
+    public $payment_type = "1"; //必填，不能修改
+    //服务器异步通知页面路径
+    public $notify_url = "http://商户网关地址/create_direct_pay_by_user-PHP-UTF-8/notify_url.php"; //需http://格式的完整路径，不能加?id=123这类自定义参数
 
-//支付类型
-    public $payment_type = "1";
-//必填，不能修改
-//服务器异步通知页面路径
-    //public $notify_url = \Yii::$app->params['notifyUrl'];
-//需http://格式的完整路径，不能加?id=123这类自定义参数
-//页面跳转同步通知页面路径
-    //public $return_url = \Yii::$app->params['returnUrl'];
-//需http://格式的完整路径，不能加?id=123这类自定义参数，不能写成http://localhost/
-//必填
-//商品数量
-    public $quantity = "1";
-//必填，建议默认为1，不改变值，把一次交易看成是一次下订单而非购买一件商品
-//物流费用
-    public $logistics_fee = "0.00";
-//必填，即运费
-//物流类型
-    public $logistics_type = "EXPRESS";
-//必填，三个值可选：EXPRESS（快递）、POST（平邮）、EMS（EMS）
-//物流支付方式
-    public $logistics_payment = "SELLER_PAY";
-//必填，两个值可选：SELLER_PAY（卖家承担运费）、BUYER_PAY（买家承担运费）
-//订单描述
-//商户订单号
+    //页面跳转同步通知页面路径
+    public $return_url = "http://商户网关地址/create_direct_pay_by_user-PHP-UTF-8/return_url.php"; //需http://格式的完整路径，不能加?id=123这类自定义参数，不能写成http://localhost/
 
+    //商户订单号
+    public $out_trade_no = ""; //商户网站订单系统中唯一订单号，必填
+
+    //订单名称
+    public $subject = ""; //必填
+
+    //付款金额
+    public $total_fee = ""; //必填
+
+    //订单描述
+    public $body = "";
+    //商品展示地址
+    public $show_url = ""; //需以http://开头的完整路径，例如：http://www.商户网址.com/myorder.html
+
+    //防钓鱼时间戳
+    public $anti_phishing_key = ""; //若要使用请调用类文件submit中的query_timestamp函数
+
+    //客户端的IP地址
+    public $exter_invoke_ip = ""; //非局域网的外网IP地址，如：221.0.0.1
 
     public $parameter = [];
     public $alipay_config = [];
 
-    public function __construct($out_trade_no, $subject, $price, $body, $show_url, $receive_name, $receive_address, $receive_zip, $receive_phone, $receive_mobile) {
-   
-        //↓↓↓↓↓↓↓↓↓↓请在这里配置您的基本信息↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
-        //合作身份者id，以2088开头的16位纯数字
+    public function __construct($notifyUrl, $returnUrl, $out_trade_no, $subject, $total_fee, $body, $show_url) {
         $this->alipay_config=(new Aliconfig)->getAliconfig();
 
         $this->parameter = array(
-            "service" => "create_partner_trade_by_buyer",
-            "partner" => trim($this->alipay_config['partner']),
-            "seller_email" => trim($this->alipay_config['seller_email']),
-            "payment_type" => $this->payment_type,
-            "notify_url" => \Yii::$app->params['notifyUrl'],
-            "return_url" => \Yii::$app->params['returnUrl'],
-            "out_trade_no" => $out_trade_no,
-            "subject" => $subject,
-            "price" => $price,
-            "quantity" => $this->quantity,
-            "logistics_fee" => $this->logistics_fee,
-            "logistics_type" => $this->logistics_type,
-            "logistics_payment" => $this->logistics_payment,
-            "body" => $body,
-            "show_url" => $show_url,
-            "receive_name" => $receive_name,
-            "receive_address" => $receive_address,
-            "receive_zip" => $receive_zip,
-            "receive_phone" => $receive_phone,
-            "receive_mobile" => $receive_mobile,
-            "_input_charset" => trim(strtolower($this->alipay_config['input_charset']))
+            "service"           => "create_direct_pay_by_user",
+            "partner"           => trim($this->alipay_config['partner']),
+            "seller_email"      => trim($this->alipay_config['seller_email']),
+            "payment_type"      => $this->payment_type,
+            "notify_url"        => $notifyUrl,
+            "return_url"        => $returnUrl,
+            "out_trade_no"      => $out_trade_no,
+            "subject"           => $subject,
+            "total_fee"	        => $total_fee,
+            "body"	            => $body,
+            "show_url"	        => $show_url,
+            "anti_phishing_key"	=> $this->anti_phishing_key,
+            "exter_invoke_ip"	=> $this->exter_invoke_ip,
+            "_input_charset"    => trim(strtolower($this->alipay_config['input_charset'])),
         );
-        
     }
-
-//$out_trade_no = $_POST['WIDout_trade_no'];
-////商户网站订单系统中唯一订单号，必填
-////订单名称
-//$subject = $_POST['WIDsubject'];
-////必填
-////付款金额
-//$price = $_POST['WIDprice'];
-//
-//$body = $_POST['WIDbody'];
-////商品展示地址
-//$show_url = $_POST['WIDshow_url'];
-////需以http://开头的完整路径，如：http://www.商户网站.com/myorder.html
-////收货人姓名
-//$receive_name = $_POST['WIDreceive_name'];
-////如：张三
-////收货人地址
-//$receive_address = $_POST['WIDreceive_address'];
-////如：XX省XXX市XXX区XXX路XXX小区XXX栋XXX单元XXX号
-////收货人邮编
-//$receive_zip = $_POST['WIDreceive_zip'];
-////如：123456
-////收货人电话号码
-//$receive_phone = $_POST['WIDreceive_phone'];
-////如：0571-88158090
-////收货人手机号码
-//$receive_mobile = $_POST['WIDreceive_mobile'];
-//如：13312341234
-
-    /*     * ********************************************************* */
-
-//构造要请求的参数数组，无需改动
 }
-
-//建立请求
-//$alipaySubmit = new AlipaySubmit($this->alipay_config);
-//$html_text = $alipaySubmit->buildRequestForm($this->parameter, "get", "确认");
-//echo $html_text;
 
